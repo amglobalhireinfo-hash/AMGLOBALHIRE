c2b76e4c724d7e85cd24981d6e5af1e3c75d843f
amglobalhireinfo-hash <amglobalhireinfo@gmail.com>
2026-04-04 18:42:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -95,3 +95,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// Scroll Animation
+window.addEventListener("scroll", function(){
+  let reveals = document.querySelectorAll(".reveal");
+  for(let i=0;i<reveals.length;i++){
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 100;
+    if(elementTop < windowHeight - elementVisible){
+      reveals[i].classList.add("active");
+    }
+  }
+});
